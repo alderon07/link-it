51079e84bd74511229f8ef216f369634b68230b7
Naqi <naqihaider07@gmail.com>
2025-05-22 16:26:26 -0400

    feat(layout): wrap application in ClerkProvider for authentication context
    - Integrated ClerkProvider into the RootLayout component to enable authentication context throughout the application.
    - Ensured proper structure of the HTML and body elements for consistent rendering.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/ui/navbar/Navbar";
 import { ReactScan } from "@/components/ReactScan";
+import {ClerkProvider} from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,12 +18,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="min-h-screen">
-      <body className={`${inter.className} min-h-screen pt-14 bg-background text-text`}>
-        <ReactScan />
-        <Navbar />
-        {children}
-      </body>
-    </html>
+      <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} min-h-screen pt-14 bg-background text-text`}>
+          <ReactScan />
+          <Navbar />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
